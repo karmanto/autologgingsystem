5a29bfe63ffae120e0d33960366d785d44a21b89
karmanto <karmanto.s@gmail.com>
2024-07-03 19:17:49 +0700

    auth

---
+++ app/Http/Middleware/RoleMiddleware.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Auth;
+
+class RoleMiddleware
+{
+    public function handle($request, Closure $next, $role)
+    {
+        if (!Auth::check()) {
+            return redirect('/login');
+        }
+
+        $user = Auth::user();
+        if ($user->role !== $role) {
+            return redirect('/home');
+        }
+
+        return $next($request);
+    }
+}
+
