399240a439dbdfbb648e30b3574012c8f6d51b97
karmanto <karmanto.s@gmail.com>
2024-07-06 22:34:11 +0700

    test python7

--- python/config.py
+++ python/config.py
@@ -5,14 +5,15 @@ import digitalio
 import os
 from datetime import datetime, date, timedelta
 from dotenv import load_dotenv
+from adafruit_mcp230xx.mcp23017 import MCP23017
 
 load_dotenv()
 
-FIELD_TABLE_ARRAY = ["cbc1","cbc2","prs1","prs2","prs3","prs4","prs5","prs6","prs7","prs8","dtr1","dtr2","dtr3","dtr4","dtr5","dtr6","dtr7","dtr8", "spr0","spr1","spr2","spr3","spr4","spr5","spr6","spr7","spr8","spr9","spr10","spr11","spr12","spr13","spr14","spr15","spr16","spr17","spr18","spr19","spr20","spr21","spr22","spr23","spr24","spr25","spr, 26","spr27","spr28","spr29","spr30","spr31","spr32","spr33","spr34","spr35","spr36","spr37","spr38","spr39","spr40","spr41","spr42","spr43","spr44","spr45"]
-OUT1 = board.D23
-OUT2 = board.D24
-OUT3 = board.D4
-RPI_PIN_ARRAY = [
+_I2C_PIN = busio.I2C(board.SCL, board.SDA)
+_OUT1 = board.D23
+_OUT2 = board.D24
+_OUT3 = board.D4
+_RPI_PIN_ARRAY = [
     board.D5, board.D6, board.D7, board.D8,
     board.D9, board.D10, board.D11, board.D12,
     board.D13, board.D27, board.D25, board.D16,
@@ -20,8 +21,9 @@ RPI_PIN_ARRAY = [
     board.D21, board.D22
 ]
 
-RPI_OUTPUT_ARRAY = [digitalio.DigitalInOut(pin) for pin in [OUT1, OUT2, OUT3]]
-RPI_INPUT_ARRAY = [digitalio.DigitalInOut(pin) for pin in RPI_PIN_ARRAY]
+FIELD_TABLE_ARRAY = ["cbc1","cbc2","prs1","prs2","prs3","prs4","prs5","prs6","prs7","prs8","dtr1","dtr2","dtr3","dtr4","dtr5","dtr6","dtr7","dtr8", "spr0","spr1","spr2","spr3","spr4","spr5","spr6","spr7","spr8","spr9","spr10","spr11","spr12","spr13","spr14","spr15","spr16","spr17","spr18","spr19","spr20","spr21","spr22","spr23","spr24","spr25","spr, 26","spr27","spr28","spr29","spr30","spr31","spr32","spr33","spr34","spr35","spr36","spr37","spr38","spr39","spr40","spr41","spr42","spr43","spr44","spr45"]
+RPI_OUTPUT_ARRAY = [digitalio.DigitalInOut(pin) for pin in [_OUT1, _OUT2, _OUT3]]
+RPI_INPUT_ARRAY = [digitalio.DigitalInOut(pin) for pin in _RPI_PIN_ARRAY]
 
 def get_db_connection():
     return MySQLdb.connect(
@@ -38,4 +40,18 @@ def init_rpi_gpio():
 
     for INPUT in RPI_INPUT_ARRAY:
         INPUT.direction = digitalio.Direction.INPUT
-        INPUT.pull = digitalio.Pull.UP
+        INPUT.pull = digitalio.Pull.DOWN
+
+def init_mcp(ADDR):
+    try:
+        MCP = MCP23017(_I2C_PIN, address=ADDR)
+        PINS = [MCP.get_pin(i) for i in range(16)]
+        
+        for PIN in PINS:
+            PIN.direction = digitalio.Direction.INPUT
+            PIN.pull = digitalio.Pull.UP
+        
+        return True, PINS
+
+    except:
+        return False, []
--- python/main.py
+++ python/main.py
@@ -3,12 +3,21 @@ import time
 from time import sleep
 
 config.init_rpi_gpio()
+MCP1_INIT, MCP1_PINS = config.init_mcp(0x20)
+MCP2_INIT, MCP2_PINS = config.init_mcp(0x21)
+MCP3_INIT, MCP3_PINS = config.init_mcp(0x22)
 
 while True:
 #    time.sleep(.5)
 #    config.RPI_OUTPUT_ARRAY[1].value = True
 #    time.sleep(.5)
 #    config.RPI_OUTPUT_ARRAY[1].value = False
-    input_values = [INPUT_PIN.value for INPUT_PIN in config.RPI_INPUT_ARRAY]
-    print(f"\rInput pin values: {input_values}", end='', flush=True)
+#    input_values = [INPUT_PIN.value for INPUT_PIN in config.RPI_INPUT_ARRAY]
+#    print(f"\rInput pin values: {input_values}", end='', flush=True)
+#    time.sleep(1)
+    if MCP1_INIT :
+        MCP_VALUES = [PIN.value for PIN in MCP1_PINS] 
+        print(f"\rMCP pin values: {MCP_VALUES}", end='', flush=True)
+
     time.sleep(1)
+
