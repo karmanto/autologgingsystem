2575e286f1931cb0df99637216029f8f290d3f49
karmanto <karmanto.s@gmail.com>
2024-07-07 16:33:02 +0700

    test python add data monitor1

--- python/main.py
+++ python/main.py
@@ -6,33 +6,40 @@ config.init_rpi_gpio()
 MCP1_INIT, MCP1_PINS = config.init_mcp(0x20)
 MCP2_INIT, MCP2_PINS = config.init_mcp(0x21)
 MCP3_INIT, MCP3_PINS = config.init_mcp(0x22)
+config.RPI_OUTPUT_ARRAY[0].value = True
 DB_INIT, DB = config.get_db_connection()
 
+while not DB_INIT:
+    time.sleep(5)
+    DB_INIT, DB = config.get_db_connection()
+
+CURSOR = DB.cursor()
+
 while True:
-   time.sleep(1)
-   INPUTS = [True] * 66
-   config.RPI_OUTPUT_ARRAY[0].value = not config.RPI_OUTPUT_ARRAY[0].value
-   for index, INPUT_PIN in enumerate(config.RPI_INPUT_ARRAY):
-    INPUTS[index] = INPUT_PIN.value
-    
-    if MCP1_INIT :
+    time.sleep(1)
+    INPUTS = [True] * 66
+    config.RPI_OUTPUT_ARRAY[0].value = not config.RPI_OUTPUT_ARRAY[0].value
+    for index, INPUT_PIN in enumerate(config.RPI_INPUT_ARRAY):
+        INPUTS[index] = not INPUT_PIN.value
+
+    if MCP1_INIT:
         for index, INPUT_PIN in enumerate(MCP1_PINS):
-            INPUTS[index + 18] = INPUT_PIN.value
+            INPUTS[index + 18] = not INPUT_PIN.value
 
-    if MCP2_INIT :
+    if MCP2_INIT:
         for index, INPUT_PIN in enumerate(MCP2_PINS):
-            INPUTS[index + 18 + 16] = INPUT_PIN.value
+            INPUTS[index + 18 + 16] = not INPUT_PIN.value
 
-    if MCP3_INIT :
+    if MCP3_INIT:
         for index, INPUT_PIN in enumerate(MCP3_PINS):
-            INPUTS[index + 18 + 16 + 16] = INPUT_PIN.value
-    
-    print(f"\rInput pin values: {INPUTS}", end='', flush=True)
+            INPUTS[index + 18 + 16 + 16] = not INPUT_PIN.value
 
-    #db = config.get_db_connection()
-    #cursor = db.cursor()
-    #cursor.execute("SELECT VERSION()")
-    #result = cursor.fetchone()
-    #print(f"Database version: {result[0]}")
-    #cursor.close()
-    #db.close()
+    # Create placeholders for the query
+    placeholders = ','.join(['%s'] * 45)
+    fields = ','.join(config.FIELD_TABLE_ARRAY[:45])
+
+    CURSOR.execute(
+        f"INSERT INTO data_monitor ({fields}) VALUES ({placeholders})",
+        tuple(INPUTS[:45])
+    )
+    DB.commit()
