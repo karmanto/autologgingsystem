5dc717f22f9890cbc68891947c82ec4522f2f4ac
karmanto <karmanto.s@gmail.com>
2024-07-08 07:24:12 +0700

    test python commit db

--- python/main.py
+++ python/main.py
@@ -17,7 +17,7 @@ CURSOR = DB.cursor()
 
 while True:
     time.sleep(1)
-    INPUTS = [True] * 66
+    INPUTS = [False] * 66
     config.RPI_OUTPUT_ARRAY[0].value = not config.RPI_OUTPUT_ARRAY[0].value
     for index, INPUT_PIN in enumerate(config.RPI_INPUT_ARRAY):
         INPUTS[index] = not INPUT_PIN.value
@@ -34,12 +34,16 @@ while True:
         for index, INPUT_PIN in enumerate(MCP3_PINS):
             INPUTS[index + 18 + 16 + 16] = not INPUT_PIN.value
 
-    # Create placeholders for the query
-    placeholders = ','.join(['%s'] * 45)
-    fields = ','.join(config.FIELD_TABLE_ARRAY[:45])
-
-    CURSOR.execute(
-        f"INSERT INTO data_monitor ({fields}) VALUES ({placeholders})",
-        tuple(INPUTS[:45])
-    )
-    DB.commit()
+    cur.execute	(	"INSERT INTO data_monitor (" + config.FIELD_STRING + ") " +
+					"VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)", 
+                    (
+                    INPUTS[0], INPUTS[1], INPUTS[2], INPUTS[3], INPUTS[4], INPUTS[5], INPUTS[6], INPUTS[7], INPUTS[8], INPUTS[9], 
+                    INPUTS[10], INPUTS[11], INPUTS[12], INPUTS[13], INPUTS[14], INPUTS[15], INPUTS[16], INPUTS[17], INPUTS[18], INPUTS[19], 
+                    INPUTS[20], INPUTS[21], INPUTS[22], INPUTS[23], INPUTS[24], INPUTS[25], INPUTS[26], INPUTS[27], INPUTS[28], INPUTS[29], 
+                    INPUTS[30], INPUTS[31], INPUTS[32], INPUTS[33], INPUTS[34], INPUTS[35], INPUTS[36], INPUTS[37], INPUTS[38], INPUTS[39], 
+                    INPUTS[40], INPUTS[41], INPUTS[42], INPUTS[43], INPUTS[44], INPUTS[45], INPUTS[46], INPUTS[47], INPUTS[48], INPUTS[49], 
+                    INPUTS[50], INPUTS[51], INPUTS[52], INPUTS[53], INPUTS[54], INPUTS[55], INPUTS[56], INPUTS[57], INPUTS[58], INPUTS[59], 
+                    INPUTS[60], INPUTS[61], INPUTS[62], INPUTS[63]
+                    )
+                )
+    db.commit()	
