b54498c7b5e3d4a0b9e415c1b04c676ca9edec1a
karmanto <karmanto.s@gmail.com>
2024-07-10 13:25:33 +0700

    update sh

--- sh/run.sh
+++ sh/run.sh
@@ -1,3 +1,5 @@
+NGROK_STATIC_CMD=$(grep NGROK_STATIC_CMD ./.env | cut -d '=' -f 2-)
+
 rm ./storage/app/monitor.json && 
 ./python/venv/bin/python ./python/main.py &
-ngrok http --domain=open-useful-joey.ngrok-free.app 80
+$NGROK_STATIC_CMD
