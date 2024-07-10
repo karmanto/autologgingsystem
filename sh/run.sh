b97465db9a109619132a9c424237a1e90c71a6f4
karmanto <karmanto.s@gmail.com>
2024-07-10 14:04:11 +0700

    update sh

--- sh/run.sh
+++ sh/run.sh
@@ -2,4 +2,7 @@ NGROK_STATIC_CMD=$(grep NGROK_STATIC_CMD ./.env | cut -d '=' -f 2- | tr -d '"')
 
 rm ./storage/app/monitor.json && 
 ./python/venv/bin/python ./python/main.py &
+
+sleep 30
+
 ${NGROK_STATIC_CMD}
