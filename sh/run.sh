ff68b1348e5ebbab71c578b53eec011672cf62b0
karmanto <karmanto.s@gmail.com>
2024-07-10 12:36:34 +0700

    update sh

--- sh/run.sh
+++ sh/run.sh
@@ -1,3 +1,3 @@
-rm ./storage/app/monitor.json & 
-./python/venv/bin/python ./python/main.py
+rm ./storage/app/monitor.json && 
+./python/venv/bin/python ./python/main.py &
 ngrok http --domain=open-useful-joey.ngrok-free.app 80
