2f6200b4791560e626fe6318b3facdc310813c44
karmanto <karmanto.s@gmail.com>
2024-07-18 20:21:36 +0700

    run ngrok securely

--- sh/run_ngrok.sh
+++ sh/run_ngrok.sh
@@ -4,6 +4,6 @@ NGROK_FILE="../storage/app/ngrok_static.txt"
 
 if [ -s "$NGROK_FILE" ]; then
     FIRST_LINE=$(sed -n '1p' "$NGROK_FILE")
-    ${FIRST_LINE}
+    /usr/local/bin/ngrok ${FIRST_LINE}
 fi
 
