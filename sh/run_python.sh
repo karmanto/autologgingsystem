e6b08a2c61f837d35abec9f7a15e8563571d5235
karmanto <karmanto.s@gmail.com>
2024-07-19 09:34:57 +0700

    disable telegram

--- sh/run_python.sh
+++ sh/run_python.sh
@@ -1,4 +1,3 @@
 rm -f ../storage/app/monitor.json && 
 sudo ../python/venv/bin/python ../python/main.py & 
 sudo ../python/venv/bin/python ../python/scan_online.py &
-sudo ../python/venv/bin/python ../python/telegram_bot.py &
